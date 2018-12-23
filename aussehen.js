238c354cf43d3ead864d9747cea876faa149ce67
Dunja <dunjawohlers@gmx.de>
2018-12-23 17:17:05 +0100

    styles hinzugefügt, hilfs-coloring

--- aussehen.js
+++ aussehen.js
@@ -1,8 +1,6 @@
 coloring()
 
 function coloring(){
-
-
 	//Baumdiagramm allgemeines Layout:
 	//Layout---------------------------------------------------------------------------------------------------
 	$("#mySVG").children().css("font-size","20");
@@ -17,73 +15,125 @@ function coloring(){
 	$("path").css("stroke", "darkgrey");
 //-----------------------------------------------------------------------------
 	//Aufgaben-Farbe:
-	$(".ungerade").addClass("lightgrey");
-	$(".ungerade").children().addClass("lightgrey");
 
-	$(".gerade").addClass("lightergrey");
-	$(".gerade").children().addClass("lightergrey");
 
-	$(".main").children().eq(1).addClass("lightgrey");
-
-//vierfelder hintergrund:
-	$(".gridContainer").children().eq(1).addClass("lightgrey");
-		$(".intro").addClass("darkgrey");
-
+vierfarbengeben("verydarkgrey", "darkgrey", "lightgrey", "lightergrey");
 $("div").addClass("rund");
 $("p").addClass("rund");
 $("h3").addClass("rund");
 
-$("h1").addClass("verydarkgrey");
-$("h2").addClass("verydarkgrey");
-$("h3").addClass("verydarkgrey");
+
 $("h1").addClass("rund");
 $("h2").addClass("rund");
 $("h3").addClass("rund");
 //$(".aufgabenübersicht").children().addClass("verydarkgrey");
-//Vierfeldertafel färben:
-//$(".ü").addClass("blue");
-//$(".summe").addClass("blue");
-//$(".mittelfeld").addClass("orange");
-//$(".rechtsfeld").addClass("green");
-//$(".untenfeld").addClass("red");
 
-//Baumdiagramm färben:
-//$("text").filter("#baumStamm").css("fill", "#0A450F");//grün
-//$("text").filter("#baumStamm").css("fill", "#750900"); //rot
-//$("text").filter("#baumAst").css("fill", "darkmagenta");
-//$("text").filter("#baumErgebnis").css("fill", "#B78800");
 
 //Baum-Hintergrund:
 $("#mySVG").addClass("rund");
-//allgemeiner Hintergrund:
-$("div").addClass("darkgrey");
-
-$(".lösungen").addClass("red");
-
 
 
 }
 
+function vierfarbengeben(dunkel2, dunkel1, hell1, hell2){
+	$("button").addClass(dunkel2);
+	$(".ungerade").addClass(hell1);
+	$(".ungerade").children().addClass(hell1);
+	$(".main").children().eq(1).addClass(hell1);
 
-	function myFunction() {
-var hintergrund = $("div").attr('class');
-$("div").removeClass("main");
-$("div").removeClass("aufträge");
+	$(".gerade").addClass(hell2);
+	$(".gerade").children().addClass(hell2);
 
+//vierfelder hintergrund:
+	$(".gridContainer").children().eq(1).addClass(hell1);
+		$(".intro").addClass(dunkel1);
 
-$(".gerade").addClass("red");
-$(".ungerade").addClass("yellow");
-$("div").addClass("red");
-$("h1").addClass("lila");
-$("h2").addClass("lila");
-$("h3").addClass("lila");
-//alert(hintergrund);
-if(hintergrund=='page rund darkgrey red'){
-  $(".gerade").removeClass("red");
-  $(".ungerade").removeClass("yellow");
-	$("div").removeClass("red");
-	$("h1").removeClass("lila");
-	$("h2").removeClass("lila");
-	$("h3").removeClass("lila");
+		$("h1").addClass(dunkel2);
+		$("h2").addClass(dunkel2);
+		$("h3").addClass(dunkel2);
+
+		//allgemeiner Hintergrund:
+		$("div").addClass(dunkel1);
+
 }
+
+function vierfarbenlöschen(dunkel2, dunkel1, hell1, hell2){
+		$("button").removeClass(dunkel2);
+	$(".ungerade").removeClass(hell1);
+	$(".ungerade").children().removeClass(hell1);
+	$(".main").children().eq(1).removeClass(hell1);
+
+	$(".gerade").removeClass(hell2);
+	$(".gerade").children().removeClass(hell2);
+
+//vierfelder hintergrund:
+	$(".gridContainer").children().eq(1).removeClass(hell1);
+		$(".intro").removeClass(dunkel1);
+
+		$("h1").removeClass(dunkel2);
+		$("h2").removeClass(dunkel2);
+		$("h3").removeClass(dunkel2);
+
+		//allgemeiner Hintergrund:
+		$("div").removeClass(dunkel1);
+
+}
+
+var clicker =0;
+	function layOut() {
+		clicker++;
+		if(clicker==1){
+vierfarbenlöschen("verydarkgrey", "darkgrey", "lightgrey", "lightergrey");
+vierfarbengeben("verydarkblue", "darkblue", "lightblue", "lighterblue");
+}
+else if(clicker==2){
+	vierfarbenlöschen("verydarkblue", "darkblue", "lightblue", "lighterblue");
+	vierfarbengeben("verydarklila", "darklila", "lightlila", "lighterlila");
+}
+
+
+
+
+
+
+	else if(clicker==3){
+		vierfarbenlöschen("verydarklila", "darklila", "lightlila", "lighterlila");
+	vierfarbengeben("verydarkgrey", "darkgrey", "lightgrey", "lightergrey");
+	clicker=0;
+	}
+
+}
+
+
+function myFunction() {
+	var hintergrund= 	$(".mittelfeld").attr("class");
+	//alert(hintergrund);
+ if(hintergrund.indexOf("orange")!=-1){ //string enthält den string orange
+	 //Vierfeldertafel färben:
+	 $(".ü").removeClass("blue");
+	 $(".summe").removeClass("blue");
+	 $(".mittelfeld").removeClass("orange");
+	 $(".rechtsfeld").removeClass("green");
+	 $(".untenfeld").removeClass("red");
+
+	 //Baumdiagramm färben:
+	 $("text").css("fill", "darkgrey");
+ }
+
+else{
+	//Vierfeldertafel färben:
+	$(".ü").addClass("blue");
+	$(".summe").addClass("blue");
+	$(".mittelfeld").addClass("orange");
+	$(".rechtsfeld").addClass("green");
+	$(".untenfeld").addClass("red");
+
+	//Baumdiagramm färben:
+	$("text").filter("#baumStamm").css("fill", "#0A450F");//grün
+	$("text").filter("#baumStamm").css("fill", "#750900"); //rot
+	$("text").filter("#baumAst").css("fill", "darkmagenta");
+	$("text").filter("#baumErgebnis").css("fill", "#B78800");
+}
+
+
 }
