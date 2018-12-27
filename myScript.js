9d08b0b9a8574ff339562d00a6949a045e64620e
Dunja <dunjawohlers@gmx.de>
2018-12-27 15:52:00 +0100

    jo

--- myScript.js
+++ myScript.js
@@ -94,7 +94,7 @@ function vierfeldertafelReset (){
 	$("#vierfelderBox").children().eq(2).text("\\(\\bar{\\text{A}}\\)");
 	$("#vierfelderBox").children().eq(3).text(summe);
 	$("#vierfelderBox").children().eq(4).text("\\(\\text{B}\\)");
-	$("#vierfelderBox").children().eq(5).html("<span class='.MathJax.merror'> \\[\\text{P(A}\\cap \\text{B)}\\]  </span>");
+	$("#vierfelderBox").children().eq(5).html("<span id='myeqn'> \\[\\text{P(A}\\cap \\text{B)}\\]  </span>");
 	$("#vierfelderBox").children().eq(6).html("<span id='myeqn'> \\(	\\text{P(}\\bar{\\text{A}} \\cap \\text{B)}\\) </span>");
 	$("#vierfelderBox").children().eq(7).text("\\(\\text{P(B)}\\)");
 	$("#vierfelderBox").children().eq(8).text("\\(	\\bar{\\text{B}}\\)");
@@ -105,7 +105,6 @@ function vierfeldertafelReset (){
 	$("#vierfelderBox").children().eq(13).text("\\(\\text{P(A)}\\)");
 	$("#vierfelderBox").children().eq(14).text("\\(\\text{P(}\\bar{\\text{A}}\\text{)}\\)");
 	$("#vierfelderBox").children().eq(15).text("1");
-	$("#vierfelderBox").children().css("width","100%");
 }
 
 //VierfelderTafel Input:
